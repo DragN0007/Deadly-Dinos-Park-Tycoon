a83a4cd7207a8e2f93ce7ce02616753f380ed45b
kylie <cooper.kylie1077@gmail.com>
2024-08-15 02:15:58 -0500

    -Init

--- src/main/java/com/dragn0007/deadlydinospt/item/custom/DinoEggItem.java
+++ src/main/java/com/dragn0007/deadlydinospt/item/custom/DinoEggItem.java
@@ -2,22 +2,33 @@ package com.dragn0007.deadlydinospt.item.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.ForgeSpawnEggItem;
+import net.minecraftforge.fluids.FluidAttributes;
 
 import java.util.Objects;
 import java.util.function.Supplier;
@@ -30,8 +41,16 @@ public class DinoEggItem extends ForgeSpawnEggItem {
     @Override
     public InteractionResult useOn(UseOnContext context) {
         Level level = context.getLevel();
+        BlockPos pos = context.getClickedPos();
+        BlockState state = level.getBlockState(pos);
+
+        if (state.is(Blocks.WATER)) {
+            return InteractionResult.FAIL;
+        }
+
         if (!(level instanceof ServerLevel)) {
             return InteractionResult.SUCCESS;
+
         } else {
             ItemStack itemstack = context.getItemInHand();
             BlockPos blockpos = context.getClickedPos();
@@ -68,4 +87,5 @@ public class DinoEggItem extends ForgeSpawnEggItem {
             return InteractionResult.CONSUME;
         }
     }
+
 }

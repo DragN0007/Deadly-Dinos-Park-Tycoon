9c2eecc6209e308b3fa8916715ea0485bbc04a63
kylie <cooper.kylie1077@gmail.com>
2024-08-20 01:23:46 -0500

    -Init

--- src/main/java/com/dragn0007/deadlydinospt/util/config/DeadlyDinosPTCommonConfig.java
+++ src/main/java/com/dragn0007/deadlydinospt/util/config/DeadlyDinosPTCommonConfig.java
@@ -108,34 +108,27 @@ public class DeadlyDinosPTCommonConfig {
                         "Bones generally have a higher success rate, as they're fresher.")
                 .define("Acid Vat success rate percent: Bones", 50);
 
-        DNA_EXTRACTOR_BONE_SUCCESS_RATE = BUILDER.comment("DNA Extractor success rate, as a percentage. Default is 50. " +
-                        "Bones generally have a higher success rate, as they're fresher.")
+        DNA_EXTRACTOR_BONE_SUCCESS_RATE = BUILDER.comment("DNA Extractor success rate, as a percentage. Default is 50.")
                 .define("DNA Extractor success rate percent: Bones", 50);
 
-        EMBRYO_INITIATOR_BONE_SUCCESS_RATE = BUILDER.comment("Embryo Initiator success rate, as a percentage. Default is 50. " +
-                        "Bones generally have a higher success rate, as they're fresher.")
+        EMBRYO_INITIATOR_BONE_SUCCESS_RATE = BUILDER.comment("Embryo Initiator success rate, as a percentage. Default is 50.")
                 .define("Embryo Initiator success rate percent: Bones", 50);
 
-        EMBRYO_INJECTOR_BONE_SUCCESS_RATE = BUILDER.comment("Embryo Injector success rate, as a percentage. Default is 85. " +
-                        "Bones generally have a higher success rate, as they're fresher.")
+        EMBRYO_INJECTOR_BONE_SUCCESS_RATE = BUILDER.comment("Embryo Injector success rate, as a percentage. Default is 85.")
                 .define("Embryo Injector success rate percent: Bones", 85);
-        BUILDER.pop();
 
 
         ACID_VAT_FOSSIL_SUCCESS_RATE = BUILDER.comment("Acid Vat success rate, as a percentage. Default is 33. " +
                         "Fossils generally have a lower success rate, as they're much older.")
                 .define("Acid Vat success rate percent: Fossils", 33);
 
-        DNA_EXTRACTOR_FOSSIL_SUCCESS_RATE = BUILDER.comment("DNA Extractor success rate, as a percentage. Default is 33. " +
-                        "Fossils generally have a lower success rate, as they're much older.")
+        DNA_EXTRACTOR_FOSSIL_SUCCESS_RATE = BUILDER.comment("DNA Extractor success rate, as a percentage. Default is 33.")
                 .define("DNA Extractor success rate percent: Fossils", 33);
 
-        EMBRYO_INITIATOR_FOSSIL_SUCCESS_RATE = BUILDER.comment("Embryo Initiator success rate, as a percentage. Default is 25. " +
-                        "Fossils generally have a lower success rate, as they're much older.")
+        EMBRYO_INITIATOR_FOSSIL_SUCCESS_RATE = BUILDER.comment("Embryo Initiator success rate, as a percentage. Default is 25.")
                 .define("Embryo Initiator success rate percent: Fossils", 25);
 
-        EMBRYO_INJECTOR_FOSSIL_SUCCESS_RATE = BUILDER.comment("Embryo Injector success rate, as a percentage. Default is 75. " +
-                        "Fossils generally have a lower success rate, as they're much older.")
+        EMBRYO_INJECTOR_FOSSIL_SUCCESS_RATE = BUILDER.comment("Embryo Injector success rate, as a percentage. Default is 75.")
                 .define("Embryo Injector success rate percent: Fossils", 75);
         BUILDER.pop();
     }
@@ -144,178 +137,178 @@ public class DeadlyDinosPTCommonConfig {
         BUILDER.push("Fossil Spawn Weight Configs");
 
         ACRO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ACRO Fossil Spawn Weight", 1);
+                .define("Acrocanthosaurus Fossil Spawn Weight", 1);
 
         ALBERTO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ALBERTO Fossil Spawn Weight", 1);
+                .define("Albertosaurus Fossil Spawn Weight", 1);
 
         ALLO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ALLO Fossil Spawn Weight", 1);
+                .define("Allosaurus Fossil Spawn Weight", 1);
 
         ANDAL_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ANDAL Fossil Spawn Weight", 1);
+                .define("Andalgalornis Fossil Spawn Weight", 1);
 
         AMARGA_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("AMARGA Fossil Spawn Weight", 1);
+                .define("Amargasaurus Fossil Spawn Weight", 1);
 
         AMPELO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("AMPELO Fossil Spawn Weight", 1);
+                .define("Ampelosaurus Fossil Spawn Weight", 1);
 
         ANKY_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ANKY Fossil Spawn Weight", 1);
+                .define("Ankylosaurus Fossil Spawn Weight", 1);
 
         ARCHAE_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ARCHAE Fossil Spawn Weight", 1);
+                .define("Archaeopteryx Fossil Spawn Weight", 1);
 
         ARGAN_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ARGAN Fossil Spawn Weight", 1);
+                .define("Arganodus Fossil Spawn Weight", 1);
 
         ATROCI_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ATROCI Fossil Spawn Weight", 1);
+                .define("Atrociraptor Fossil Spawn Weight", 1);
 
         AUSTRALO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("AUSTRALO Fossil Spawn Weight", 1);
+                .define("Australovenator Fossil Spawn Weight", 1);
 
         AUSTRO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("AUSTRO Fossil Spawn Weight", 1);
+                .define("Austroraptor Fossil Spawn Weight", 1);
 
         AVA_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("AVA Fossil Spawn Weight", 1);
+                .define("Avaceratops Fossil Spawn Weight", 1);
 
         BARY_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("BARY Fossil Spawn Weight", 1);
+                .define("Baryonyx Fossil Spawn Weight", 1);
 
         BASILO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("BASILO Fossil Spawn Weight", 1);
+                .define("Basilosaurus Fossil Spawn Weight", 1);
 
         CARCHAR_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("CARCHAR Fossil Spawn Weight", 1);
+                .define("Carcharodontosaurus Fossil Spawn Weight", 1);
 
         CARNO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("CARNO Fossil Spawn Weight", 1);
+                .define("Carnotaurus Fossil Spawn Weight", 1);
 
         CERATO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("CERATO Fossil Spawn Weight", 1);
+                .define("Ceratosaurus Fossil Spawn Weight", 1);
 
         COMPY_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("COMPY Fossil Spawn Weight", 1);
+                .define("Compsognathus Fossil Spawn Weight", 1);
 
         CROPSNAIL_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("CROPSNAIL Fossil Spawn Weight", 1);
+                .define("Crop Snail Fossil Spawn Weight", 1);
 
         CRYO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("CRYO Fossil Spawn Weight", 1);
+                .define("Cryolophosaurus Fossil Spawn Weight", 1);
 
         DEINOCH_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("DEINOCH Fossil Spawn Weight", 1);
+                .define("Deinocherius Fossil Spawn Weight", 1);
 
         DEINON_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("DEINON Fossil Spawn Weight", 1);
+                .define("Deinonychus Fossil Spawn Weight", 1);
 
         DILO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("DILO Fossil Spawn Weight", 1);
+                .define("Dilophosaurus Fossil Spawn Weight", 1);
 
         DUNKLEO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("DUNKLEO Fossil Spawn Weight", 1);
+                .define("Dunkleosteus Fossil Spawn Weight", 1);
 
         EDMONTO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("EDMONTO Fossil Spawn Weight", 1);
+                .define("Edmontosaurus Fossil Spawn Weight", 1);
 
         EOCARCHAR_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("EOCARCHAR Fossil Spawn Weight", 1);
+                .define("Eocarcharia Fossil Spawn Weight", 1);
 
         GALLI_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("GALLI Fossil Spawn Weight", 1);
+                .define("Gallimimus Fossil Spawn Weight", 1);
 
         GAR_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("GAR Fossil Spawn Weight", 1);
+                .define("Alligator Gar Fossil Spawn Weight", 1);
 
         GIGA_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("GIGA Fossil Spawn Weight", 1);
+                .define("Giganotosaurus Fossil Spawn Weight", 1);
 
         GRYPO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("GRYPO Fossil Spawn Weight", 1);
+                .define("Gryposaurus Fossil Spawn Weight", 1);
 
         HELI_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("HELI Fossil Spawn Weight", 1);
+                .define("Helicoprion Fossil Spawn Weight", 1);
 
         HYNERIA_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("HYNERIA Fossil Spawn Weight", 1);
+                .define("Hyneria Fossil Spawn Weight", 1);
 
         ICHTHY_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ICHTHY Fossil Spawn Weight", 1);
+                .define("Ichthyovenator Fossil Spawn Weight", 1);
 
         ICHTHYO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("ICHTHYO Fossil Spawn Weight", 1);
+                .define("Ichthyosaurus Fossil Spawn Weight", 1);
 
         IGUA_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("IGUA Fossil Spawn Weight", 1);
+                .define("Iguanodon Fossil Spawn Weight", 1);
 
         LEED_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("LEED Fossil Spawn Weight", 1);
+                .define("Leedsichthys Fossil Spawn Weight", 1);
 
         LIVY_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("LIVY Fossil Spawn Weight", 1);
+                .define("Livyatan Fossil Spawn Weight", 1);
 
         MAHAKALA_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("MAHAKALA Fossil Spawn Weight", 1);
+                .define("Mahakala Fossil Spawn Weight", 1);
 
         MAJUNGA_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("MAJUNGA Fossil Spawn Weight", 1);
+                .define("Majungasaurus Fossil Spawn Weight", 1);
 
         MEG_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("MEG Fossil Spawn Weight", 1);
+                .define("Megalodon Fossil Spawn Weight", 1);
 
         MEGARAP_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("MEGARAP Fossil Spawn Weight", 1);
+                .define("Megaraptor Fossil Spawn Weight", 1);
 
         MOSA_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("MOSA Fossil Spawn Weight", 1);
+                .define("Mosasaurus Fossil Spawn Weight", 1);
 
         PACHY_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("PACHY Fossil Spawn Weight", 1);
+                .define("Pachycephalosaurus Fossil Spawn Weight", 1);
 
         PACHYR_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("PACHYR Fossil Spawn Weight", 1);
+                .define("Pachyrhino Fossil Spawn Weight", 1);
 
         PARA_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("PARA Fossil Spawn Weight", 1);
+                .define("Parasaurolophus Fossil Spawn Weight", 1);
 
         REX_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("REX Fossil Spawn Weight", 1);
+                .define("Tyrannosaurus Rex Fossil Spawn Weight", 1);
 
         SAURO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("SAURO Fossil Spawn Weight", 1);
+                .define("Sauroposeidon Fossil Spawn Weight", 1);
 
         SHANT_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("SHANT Fossil Spawn Weight", 1);
+                .define("Shantungosaurus Fossil Spawn Weight", 1);
 
         SPINO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("SPINO Fossil Spawn Weight", 1);
+                .define("Spinosaurus Fossil Spawn Weight", 1);
 
         STEGO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("STEGO Fossil Spawn Weight", 1);
+                .define("Stegosaurus Fossil Spawn Weight", 1);
 
         STETH_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("STETH Fossil Spawn Weight", 1);
+                .define("Stethacanthus Fossil Spawn Weight", 1);
 
         TARBO_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("TARBO Fossil Spawn Weight", 1);
+                .define("Tarbosaurus Fossil Spawn Weight", 1);
 
         THERI_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("THERI Fossil Spawn Weight", 1);
+                .define("Therizinosaurus Fossil Spawn Weight", 1);
 
         TRIKE_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("TRIKE Fossil Spawn Weight", 1);
+                .define("Triceratops Fossil Spawn Weight", 1);
 
         TROODON_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("TROODON Fossil Spawn Weight", 1);
+                .define("Troodon Fossil Spawn Weight", 1);
 
         UTAH_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("UTAH Fossil Spawn Weight", 1);
+                .define("Utahraptor Fossil Spawn Weight", 1);
 
         YUTY_FOSSIL_WEIGHT = BUILDER.comment("How often should this fossil spawn? Default is 1.")
-                .define("YUTY Fossil Spawn Weight", 1);
+                .define("Yutyrannus Fossil Spawn Weight", 1);
         BUILDER.pop();
 
         SPEC = BUILDER.build();
